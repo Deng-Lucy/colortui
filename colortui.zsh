2b7f85eb476e83bde6b704dc01e6d4c4077fd19b
Deng Lucy <siyudeng@ucsb.edu>
2026-04-13 14:28:56 -0700

    Update color palette: remove dark/red colors, add light pastels
    - Replace red, dark purple, dark violet with lighter alternatives
    - Add 6 new light colors: off-white, pink, mint, yellow, sky blue, lavender
    - Expand palette from 8 to 14 colors

--- colortui.zsh
+++ colortui.zsh
@@ -6,17 +6,23 @@ claude() {
   local _tty _hash _idx
   _tty=$(tty 2>/dev/null || echo "tty$$")
   _hash=$(printf '%s' "$_tty" | cksum | awk '{print $1}')
-  _idx=$((_hash % 8))
+  _idx=$((_hash % 14))
 
   local -a _fg=(
-    "rgb:CC/44/44"
+    "rgb:DD/99/33"
     "rgb:22/99/22"
     "rgb:99/88/00"
     "rgb:22/55/CC"
-    "rgb:99/22/99"
+    "rgb:CC/66/CC"
     "rgb:00/88/88"
     "rgb:CC/77/00"
-    "rgb:77/33/CC"
+    "rgb:88/99/EE"
+    "rgb:EE/EE/EE"
+    "rgb:EE/AA/BB"
+    "rgb:AA/DD/88"
+    "rgb:EE/DD/66"
+    "rgb:66/CC/EE"
+    "rgb:CC/AA/EE"
   )
 
   # Set foreground text color for the session
